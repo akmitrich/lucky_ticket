ee4214fe31f15053138dcac74c4e4b6906995722
akmitrich <ak.mitrich@mail.ru>
2022-11-04 07:57:40 +0300

    Test suite

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,25 @@
 use std::{
     collections::{hash_map::Entry, HashMap},
-    ops::{AddAssign, Mul},
+    ops::{AddAssign},
+    time::Instant,
 };
 
-pub fn lucky_with_base(n: u32, base: u32) -> u32 {
+pub fn lucky_with_base(n: u8, base: u8) -> u64 {
     assert!(base > 1);
     let sums = calc_sums(n, base);
-    sums.values().map(|x| x.mul(x)).sum()
+    sums.values().map(|x| {
+        let x = *x as u64;
+        x * x
+    }).sum()
 }
 
-pub fn lucky(n: u32) -> u32 {
+pub fn lucky(n: u8) -> u64 {
     lucky_with_base(n, 10)
 }
 
-fn calc_sums(n: u32, base: u32) -> HashMap<u32, u32> {
+fn calc_sums(n: u8, base: u8) -> HashMap<u32, u32> {
     let mut sums: HashMap<u32, u32> = HashMap::new();
-    let max_number = base.pow(n);
+    let max_number = (base as u64).pow(n as u32);
     for x in 0..max_number {
         let sum = digits_sum_with_base(x, base);
         aquire_sum(&mut sums, sum);
@@ -23,13 +27,13 @@ fn calc_sums(n: u32, base: u32) -> HashMap<u32, u32> {
     sums
 }
 
-fn digits_sum_with_base(x: u32, base: u32) -> u32 {
-    if x < base {
-        return x;
+fn digits_sum_with_base(x: u64, base: u8) -> u32 {
+    if x < base as u64 {
+        return x as u32;
     }
-    let remain = x / base;
-    let last_digit = x - remain * base;
-    last_digit + digits_sum_with_base(remain, base)
+    let remain = x / (base as u64);
+    let last_digit = x - remain * (base as u64);
+    last_digit as u32 + digits_sum_with_base(remain, base)
 }
 
 fn aquire_sum(sums: &mut HashMap<u32, u32>, sum: u32) {
@@ -47,6 +51,11 @@ mod tests {
     fn test_classic() {
         let result = dbg!(lucky(3));
         assert_eq!(55_252, result);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(670, dbg!(lucky(2)));
     }
 
     #[test]
@@ -81,7 +90,18 @@ mod tests {
     }
 
     #[test]
-    fn debug() {
-        dbg!(lucky_with_base(3, 2));
+    fn test_tickets_problem() {
+        println!("{}", include_str!("1.Tickets/problem.txt"));
+        let mut i = 0;
+        while let Ok(str_n) = std::fs::read_to_string(dbg!(format!("src/1.Tickets/test.{i}.in"))) {
+            let start = Instant::now();
+            let n = str_n.parse::<u8>().unwrap();
+            let str_answer = std::fs::read_to_string(format!("src/1.Tickets/test.{i}.out")).unwrap();
+            let str_answer = str_answer.trim();
+            assert_eq!(str_answer.parse::<u64>().unwrap(), lucky(n));
+            i += 1;
+            let _ = dbg!(Instant::now().duration_since(start));
+        }
+        assert_eq!(10, i);
     }
 }
